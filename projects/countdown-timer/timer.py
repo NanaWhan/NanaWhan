946ee9035cd5c8fece8a29ab9ac762065c02df7d
Dev_Breezy <57563850+NanaWhan@users.noreply.github.com>
2019-09-19 15:45:00 +0000

    Add pomodoro mode to countdown timer

--- projects/countdown-timer/timer.py
+++ projects/countdown-timer/timer.py
@@ -1,7 +1,8 @@
 import time
 import sys
+import os
 
-def countdown(seconds):
+def countdown(seconds, message="Time's up!"):
     while seconds > 0:
         mins, secs = divmod(seconds, 60)
         hrs, mins = divmod(mins, 60)
@@ -9,8 +10,25 @@ def countdown(seconds):
         sys.stdout.flush()
         time.sleep(1)
         seconds -= 1
-    print("\nTime's up!")
+    print(f"\n{message}")
+    try:
+        os.system('echo -e "\a"')
+    except:
+        pass
+
+def pomodoro(work=25, rest=5, cycles=4):
+    for i in range(cycles):
+        print(f"\n--- Work session {i+1}/{cycles} ---")
+        countdown(work * 60, "Take a break!")
+        if i < cycles - 1:
+            print(f"\n--- Break {i+1}/{cycles} ---")
+            countdown(rest * 60, "Back to work!")
+    print("\nAll cycles complete!")
 
 if __name__ == "__main__":
-    total = int(input("Enter seconds: "))
-    countdown(total)
+    mode = input("Mode (timer/pomodoro): ").lower()
+    if mode == "pomodoro":
+        pomodoro()
+    else:
+        total = int(input("Enter seconds: "))
+        countdown(total)
