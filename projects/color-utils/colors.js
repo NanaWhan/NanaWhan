f9287c5a9bb866c86c6293f1c9635cfaec717a9b
Dev_Breezy <57563850+NanaWhan@users.noreply.github.com>
2020-01-20 10:00:00 +0000

    Create color utility functions

---
+++ projects/color-utils/colors.js
@@ -0,0 +1,18 @@
+function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function rgbToHex(r, g, b) {
+  return '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
+}
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
+module.exports = { hexToRgb, rgbToHex, randomColor };
